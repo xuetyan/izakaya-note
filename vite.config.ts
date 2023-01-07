0a5cfe46fd6a4314864166cbbefa98d19dea1901
zhangzetian <zetian.zhang@bridgetek.cn>
2023-01-07 23:09:34 +0800

    考屁了一个打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,35 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: "dist",
+    // 9月更新
+    assetsDir: "assets", //指定静态资源存放路径
+    sourcemap: false, //是否构建source map 文件
+    // 10月更新
+    chunkSizeWarningLimit: 1500, //chunk 大小警告的限制，默认500KB
+    rollupOptions: {
+      output: {
+        // 最小化拆分包
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+        chunkFileNames: 'js/[name].[hash].js', // 用于命名代码拆分时创建的共享块的输出命名，[name]表示文件名,[hash]表示该文件内容hash值
+      }
+    },
+    terserOptions: {
+      // 生产环境移除console
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      // 10月更新
+      output: {
+         comments: true, // 去掉注释内容
+      },
+    },
   }
 })
